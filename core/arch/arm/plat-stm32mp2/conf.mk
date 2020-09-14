b48d09ce9e0438f2f5aad0f9a4f14a4b963dc094
Lionel Debieve <lionel.debieve@st.com>
2020-09-14 11:32:04 +0200

    plat-stm32mp2: add new platform support
    Initial configuration for MP2 platform.
    Add the initial memory layout and the MMU bus reference
    addresses.
    It adds also the console initialization.
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Signed-off-by: Lionel Debieve <lionel.debieve@st.com>
    Change-Id: I986656b022927634bb6d3e20f76aa3a8de25ae17

---
+++ core/arch/arm/plat-stm32mp2/conf.mk
@@ -0,0 +1,73 @@
+flavor_dts_file-257F_DK = stm32mp257f-dk.dts
+flavor_dts_file-257F_EV = stm32mp257f-ev.dts
+
+flavorlist-MP25 = $(flavor_dts_file-257F_DK) \
+		  $(flavor_dts_file-257F_EV)
+
+ifneq ($(PLATFORM_FLAVOR),)
+ifeq ($(flavor_dts_file-$(PLATFORM_FLAVOR)),)
+$(error Invalid platform flavor $(PLATFORM_FLAVOR))
+endif
+CFG_EMBED_DTB_SOURCE_FILE ?= $(flavor_dts_file-$(PLATFORM_FLAVOR))
+endif
+
+ifneq ($(filter $(CFG_EMBED_DTB_SOURCE_FILE),$(flavorlist-MP25)),)
+$(call force,CFG_STM32MP25,y)
+endif
+
+ifeq ($(filter $(CFG_STM32MP25),y),)
+$(error STM32 Platform must be defined)
+endif
+
+$(call force,CFG_ARM64_core,y)
+$(call force,CFG_WITH_LPAE,y)
+# arm-v8 platforms
+include core/arch/arm/cpu/cortex-armv8-0.mk
+$(call force,CFG_WITH_ARM_TRUSTED_FW,y)
+# Uncomment the line below to only support 64bit TA
+supported-ta-targets ?= ta_arm64
+
+$(call force,CFG_ARM_GIC_PM,y)
+$(call force,CFG_GIC,y)
+$(call force,CFG_INIT_CNTVOFF,y)
+$(call force,CFG_PM,y)
+$(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
+$(call force,CFG_DT,y)
+$(call force,CFG_GENERIC_BOOT,y)
+$(call force,CFG_DRIVERS_CLK,y)
+$(call force,CFG_DRIVERS_CLK_DT,y)
+$(call force,CFG_DRIVERS_CLK_EARLY_PROBE,y)
+$(call force,CFG_TEE_CORE_EMBED_INTERNAL_TESTS,n)
+
+ifneq ($(filter $(CFG_EMBED_DTB_SOURCE_FILE),$(flavorlist-1G)),)
+CFG_DRAM_SIZE    ?= 0x40000000
+endif
+
+CFG_DRAM_BASE ?= 0x80000000
+CFG_TZDRAM_START ?= ($(CFG_DRAM_BASE) + 0x02000000)
+CFG_TZDRAM_SIZE  ?= 0x02000000
+CFG_DRAM_SIZE    ?= 0x80000000
+
+CFG_CORE_HEAP_SIZE ?= 131072
+CFG_CORE_RESERVED_SHM ?= n
+CFG_DTB_MAX_SIZE ?= (256 * 1024)
+CFG_MMAP_REGIONS ?= 30
+CFG_NUM_THREADS ?= 5
+CFG_TEE_CORE_NB_CORE ?= 2
+
+CFG_STM32_GPIO ?= y
+CFG_STM32_SHARED_IO ?= y
+CFG_STM32_UART ?= y
+
+# Default enable some test facitilites
+CFG_TEE_CORE_EMBED_INTERNAL_TESTS ?= y
+CFG_WITH_STATS ?= y
+
+# Default disable ASLR
+CFG_CORE_ASLR ?= n
+
+# UART instance used for early console (0 disables early console)
+CFG_STM32_EARLY_CONSOLE_UART ?= 2
+
+# Default disable external DT support
+CFG_EXTERNAL_DT ?= n
