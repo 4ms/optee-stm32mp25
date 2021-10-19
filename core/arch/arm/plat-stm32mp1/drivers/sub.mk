8a08f65de50b52e1066557dc77fcbdee3e48fd30
Lionel Debieve <lionel.debieve@foss.st.com>
2021-10-19 17:16:58 +0200

    plat-stm32mp1: add calibration driver
    Add the calibration driver.
    
    Calibration driver embeds a PTA that can trigger a clock calibration when
    called. A daemon should be used to call this PTA periodically.
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@st.com>
    Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>
    Change-Id: I0d6cc78ce0bfbddc93c22ff85ec3d67e0ef3aa77
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/240881
    Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>

--- core/arch/arm/plat-stm32mp1/drivers/sub.mk
+++ core/arch/arm/plat-stm32mp1/drivers/sub.mk
@@ -1,4 +1,5 @@
 srcs-$(CFG_DDR_LOWPOWER) += stm32mp1_ddrc.c
+srcs-$(CFG_STM32_CLKCALIB) += stm32mp1_calib.c
 srcs-$(CFG_STPMIC1) += stm32mp1_pmic.c
 srcs-y 	+= stm32mp1_pwr.c
 srcs-y 	+= stm32mp1_pwr_irq.c
