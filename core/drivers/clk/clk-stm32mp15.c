570c1d7b72a05eed994474727faa476b6b1b0e2a
Gabriel Fernandez <gabriel.fernandez@foss.st.com>
2022-02-14 14:14:05 +0100

    clk: stm32mp15: remove unused data field in PLL structure
    The divn_max field was used when using the algorithm
    to check the frequency calculation of PLL1.
    
    Change-Id: I9d5b699a4be4c7795a8d6bdf1a814d904cb6eb56
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/239775
    Reviewed-by: Lionel DEBIEVE <lionel.debieve@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/288087
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
    Domain-Review: Patrick DELAUNAY <patrick.delaunay@foss.st.com>

--- core/drivers/clk/clk-stm32mp15.c
+++ core/drivers/clk/clk-stm32mp15.c
@@ -276,7 +276,6 @@ struct stm32mp1_clk_pll {
 struct stm32mp1_pll {
 	uint8_t refclk_min;
 	uint8_t refclk_max;
-	uint8_t divn_max;
 };
 
 /* Compact structure of 32bit cells, copied raw when suspending */
@@ -540,12 +539,10 @@ static const struct stm32mp1_pll stm32mp1_pll[PLL_TYPE_NB] = {
 	[PLL_800] = {
 		.refclk_min = 4,
 		.refclk_max = 16,
-		.divn_max = 99,
 	},
 	[PLL_1600] = {
 		.refclk_min = 8,
 		.refclk_max = 16,
-		.divn_max = 199,
 	},
 };
 
