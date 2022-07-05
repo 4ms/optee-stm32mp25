66f8401f9a791540012ff4154e95f65b118418b8
Etienne Carriere <etienne.carriere@linaro.org>
2022-07-05 15:46:35 +0200

    plat-stm32mp1: get DRAM size from embedded DTB
    Set CFG_DRAM_SIZE based on the embedded DTB if any. This change forces
    CFG_DRAM_SIZE to the size read from the generated embedded DTB.
    
    With this change, the value in CFG_DRAM_SIZE set in the make files
    cannot be used straight as its value may be modified when generating
    optee_os config files (header file, makefile and cmake file).
    
    Change-Id: I6beaf70a2c4af0ff04e813e4be74cf54062e320d
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/259059
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Etienne CARRIERE <etienne.carriere@foss.st.com>
    Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Christophe PRIOUZEAU <christophe.priouzeau@st.com>
    Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
    Tested-by: Etienne CARRIERE <etienne.carriere@foss.st.com>

--- core/arch/arm/plat-stm32mp1/link.mk
+++ core/arch/arm/plat-stm32mp1/link.mk
@@ -1,5 +1,26 @@
+# Specific hack for stm32mp1: get DDR size from the generated DTB to be
+# embedded in core. This force CFG_DRAM_SIZE value when build config
+# files are generated.
+
+define get_memory_node
+$(shell fdtget -l $(core-embed-fdt-dtb) / | grep memory@)
+endef
+define get_memory_size
+$(shell fdtget -t u $(core-embed-fdt-dtb) /$(get_memory_node) reg | cut -d ' ' -f 2)
+endef
+
 include core/arch/arm/kernel/link.mk
 
+all: check_build_variables
+
+check_build_variables: $(link-out-dir)/tee.elf
+	@t=`printf "%u" ${CFG_DRAM_SIZE}`; if [ $$t -ne ${get_memory_size} ]; then \
+	    t_cfg=$$(($$t / (1024*1024))); t_dtb=$$((${get_memory_size} / (1024*1024))); \
+	    echo "Wrong CFG_DRAM_SIZE $${t_cfg}MBytes, in device-tree: $${t_dtb}MBytes"; \
+	    exit 1; \
+	fi
+
+ifeq ($(CFG_STM32MP15x_STM32IMAGE),y)
 # Create stm32 formatted images from the native binary images
 
 define stm32image_cmd
