4588d641079eaa3e8eb032e12bac49c063d60644
Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
2022-07-06 18:12:18 +0200

    pta: stm32mp: add new remoteproc pta
    Add remoteproc pta skeleton inspired from the remoteproc_pta.c available
    for the stm32mp1 platform.
    
    As a first step this pta supports only the stm32mp2 relying on the
    stm32_remoteproc driver.
    
    In a second step it should also support the stm32mp1, and the legacy pta
    of the stm32mp1 platform will be removed.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/264183
    Change-Id: Ie77ad2c2dfed30f779f53df4bc0af2a4c8c32465

--- core/pta/stm32mp/sub.mk
+++ core/pta/stm32mp/sub.mk
@@ -1,1 +1,12 @@
+global-incdirs-y += .
+
 srcs-$(CFG_BSEC_PTA) += bsec_pta.c
+srcs-$(CFG_RPROC_PTA) += remoteproc_pta.c
+
+ifeq ($(CFG_RPROC_PTA),y)
+gensrcs-y += rproc_pub_key
+produce-rproc_pub_key = rproc_pub_key.c
+depends-rproc_pub_key = $(CFG_RPROC_SIGN_KEY) scripts/pem_to_pub_c.py
+recipe-rproc_pub_key = $(PYTHON3) scripts/pem_to_pub_c.py --prefix rproc_pub_key \
+        --key $(CFG_RPROC_SIGN_KEY) --out $(sub-dir-out)/rproc_pub_key.c
+endif
