f2d9fade329fbcc9d89350572afc62ecdfae7643
Pascal Paillet <p.paillet@st.com>
2022-12-01 15:44:53 +0100

    plat-stm32mp1: add high speed low voltage control
    In case of VDD low voltage, the IO boost to manage high speed
    interface is required. This must be set at boot time.
    
    Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>
    Signed-off-by: Pascal Paillet <p.paillet@st.com>
    Change-Id: I746339cc3133bcdd15d332f7c1368878a835b26b

--- core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c
+++ core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c
@@ -5,9 +5,11 @@
 
 #include <config.h>
 #include <drivers/clk.h>
+#include <drivers/regulator.h>
 #include <drivers/stm32mp_dt_bindings.h>
 #include <initcall.h>
 #include <kernel/delay.h>
+#include <kernel/pm.h>
 #include <mm/core_memprot.h>
 #include <stm32_util.h>
 #include <io.h>
@@ -29,6 +31,7 @@
 #define SYSCFG_CMPSD2CR				U(0x40)
 #define SYSCFG_CMPSD2ENSETR			U(0x44)
 #define SYSCFG_CMPSD2ENCLRR			U(0x48)
+#define SYSCFG_HSLVEN0R				U(0x50)
 #define SYSCFG_IDC				U(0x380)
 #define SYSCFG_IOSIZE				U(0x400)
 
@@ -42,6 +45,15 @@
 #define SYSCFG_SRAM3ERASER_SRAM3ER		BIT(0)
 
 #define SYSCFG_SRAM3ERASE_TIMEOUT_US		U(1000)
+
+/*
+ * SYSCFG_IOCTRLSETR Register
+ */
+#define SYSCFG_IOCTRLSETR_HSLVEN_TRACE		BIT(0)
+#define SYSCFG_IOCTRLSETR_HSLVEN_QUADSPI	BIT(1)
+#define SYSCFG_IOCTRLSETR_HSLVEN_ETH		BIT(2)
+#define SYSCFG_IOCTRLSETR_HSLVEN_SDMMC		BIT(3)
+#define SYSCFG_IOCTRLSETR_HSLVEN_SPI		BIT(4)
 
 /*
  * SYSCFG_CMPCR Register
@@ -69,6 +81,27 @@
 #define SYSCFG_IDC_DEV_ID_MASK			GENMASK_32(11, 0)
 #define SYSCFG_IDC_REV_ID_MASK			GENMASK_32(31, 16)
 #define SYSCFG_IDC_REV_ID_SHIFT			U(16)
+
+/*
+ * HSLV definitions
+ */
+#define SYSCFG_HSLV_IDX_TPIU			U(0)
+#define SYSCFG_HSLV_IDX_QSPI			U(1)
+#define SYSCFG_HSLV_IDX_ETH1			U(2)
+#define SYSCFG_HSLV_IDX_ETH2			U(3)
+#define SYSCFG_HSLV_IDX_SDMMC1			U(4)
+#define SYSCFG_HSLV_IDX_SDMMC2			U(5)
+#define SYSCFG_HSLV_IDX_SPI1			U(6)
+#define SYSCFG_HSLV_IDX_SPI2			U(7)
+#define SYSCFG_HSLV_IDX_SPI3			U(8)
+#define SYSCFG_HSLV_IDX_SPI4			U(9)
+#define SYSCFG_HSLV_IDX_SPI5			U(10)
+#define SYSCFG_HSLV_IDX_LTDC			U(11)
+#define SYSCFG_HSLV_NB_IDX			U(12)
+
+#define SYSCFG_HSLV_KEY				U(0x1018)
+
+static bool vdd_low_voltage;
 
 static vaddr_t get_syscfg_base(void)
 {
@@ -138,6 +171,39 @@ static void disable_io_compensation(int cmpcr_offset)
 
 static bool iocomp_enabled;
 
+TEE_Result stm32mp_syscfg_erase_sram3(void)
+{
+	vaddr_t base = get_syscfg_base();
+	uint64_t timeout_ref = 0;
+
+	if (!IS_ENABLED(CFG_STM32MP13))
+		return TEE_ERROR_NOT_SUPPORTED;
+
+	/* Unlock SYSCFG_SRAM3ERASER_SRAM3ER */
+	io_write32(base + SYSCFG_SRAM3KR, SYSCFG_SRAM3KR_KEY1);
+	io_write32(base + SYSCFG_SRAM3KR, SYSCFG_SRAM3KR_KEY2);
+
+	/* Request SRAM3 erase */
+	io_setbits32(base + SYSCFG_SRAM3ERASER, SYSCFG_SRAM3ERASER_SRAM3ER);
+
+	/* Lock SYSCFG_SRAM3ERASER_SRAM3ER */
+	io_write32(base + SYSCFG_SRAM3KR, 0);
+
+	/* Wait end of SRAM3 erase */
+	timeout_ref = timeout_init_us(SYSCFG_SRAM3ERASE_TIMEOUT_US);
+	while (io_read32(base + SYSCFG_SRAM3ERASER) &
+	       SYSCFG_SRAM3ERASER_SRAM3EO) {
+		if (timeout_elapsed(timeout_ref))
+			break;
+	}
+
+	/* Timeout may append due to a schedule after the while(test) */
+	if (io_read32(base + SYSCFG_SRAM3ERASER) & SYSCFG_SRAM3ERASER_SRAM3EO)
+		return TEE_ERROR_BUSY;
+
+	return TEE_SUCCESS;
+}
+
 void stm32mp_syscfg_enable_io_compensation(void)
 {
 	if (iocomp_enabled)
@@ -183,35 +249,113 @@ static TEE_Result stm32mp1_iocomp(void)
 }
 driver_init(stm32mp1_iocomp);
 
-TEE_Result stm32mp_syscfg_erase_sram3(void)
+static void enable_hslv_by_index(uint32_t index)
 {
-	vaddr_t base = get_syscfg_base();
-	uint64_t timeout_ref = 0;
+	assert(index < SYSCFG_HSLV_NB_IDX);
 
-	if (!IS_ENABLED(CFG_STM32MP13))
-		return TEE_ERROR_NOT_SUPPORTED;
-
-	/* Unlock SYSCFG_SRAM3ERASER_SRAM3ER */
-	io_write32(base + SYSCFG_SRAM3KR, SYSCFG_SRAM3KR_KEY1);
-	io_write32(base + SYSCFG_SRAM3KR, SYSCFG_SRAM3KR_KEY2);
-
-	/* Request SRAM3 erase */
-	io_setbits32(base + SYSCFG_SRAM3ERASER, SYSCFG_SRAM3ERASER_SRAM3ER);
-
-	/* Lock SYSCFG_SRAM3ERASER_SRAM3ER */
-	io_write32(base + SYSCFG_SRAM3KR, 0);
-
-	/* Wait end of SRAM3 erase */
-	timeout_ref = timeout_init_us(SYSCFG_SRAM3ERASE_TIMEOUT_US);
-	while (io_read32(base + SYSCFG_SRAM3ERASER) &
-	       SYSCFG_SRAM3ERASER_SRAM3EO) {
-		if (timeout_elapsed(timeout_ref))
-			break;
+	switch (index) {
+	case SYSCFG_HSLV_IDX_SDMMC1:
+	case SYSCFG_HSLV_IDX_SDMMC2:
+		DMSG("Not managing SDMMC HSLV");
+		break;
+	default:
+		io_write32(get_syscfg_base() + SYSCFG_HSLVEN0R +
+			   index * sizeof(uint32_t), SYSCFG_HSLV_KEY);
+		break;
 	}
+}
 
-	/* Timeout may append due to a schedule after the while(test) */
-	if (io_read32(base + SYSCFG_SRAM3ERASER) & SYSCFG_SRAM3ERASER_SRAM3EO)
-		return TEE_ERROR_BUSY;
+static void enable_high_speed_mode_low_voltage(void)
+{
+	if (IS_ENABLED(CFG_STM32MP13_CFG)) {
+		unsigned int idx = 0;
+
+		for (idx = 0; idx < SYSCFG_HSLV_NB_IDX; idx++)
+			enable_hslv_by_index(idx);
+	} else {
+		io_write32(get_syscfg_base() + SYSCFG_IOCTRLSETR,
+			   SYSCFG_IOCTRLSETR_HSLVEN_TRACE |
+			   SYSCFG_IOCTRLSETR_HSLVEN_QUADSPI |
+			   SYSCFG_IOCTRLSETR_HSLVEN_ETH |
+			   SYSCFG_IOCTRLSETR_HSLVEN_SDMMC |
+			   SYSCFG_IOCTRLSETR_HSLVEN_SPI);
+	}
+}
+
+static TEE_Result syscfg_hslv_pm(enum pm_op op, uint32_t pm_hint __unused,
+				 const struct pm_callback_handle
+				 *pm_handle __unused)
+{
+	if (op == PM_OP_RESUME && vdd_low_voltage)
+		enable_high_speed_mode_low_voltage();
 
 	return TEE_SUCCESS;
 }
+DECLARE_KEEP_PAGER(syscfg_hslv_pm);
+
+static TEE_Result stm32mp_syscfg_set_hslv(void)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	uint32_t otp_value = 0;
+	uint32_t otp_id = 0;
+	uint16_t vdd_voltage = 0;
+	bool product_below_2v5 = false;
+	struct rdev *regu = NULL;
+
+	/*
+	 * High Speed Low Voltage Pad mode Enable for SPI, SDMMC, ETH, QSPI
+	 * and TRACE. Needed above ~50MHz and conditioned by AFMUX selection.
+	 * It could be disabled for low frequencies or if AFMUX is selected
+	 * but the function is not used, typically for TRACE.
+	 * If high speed low voltage pad mode is enabled, platform will
+	 * over consume.
+	 *
+	 * WARNING:
+	 *   Enabling High Speed mode while VDD > 2.7V
+	 *   with the OTP product_below_2v5 (OTP 18, BIT 13)
+	 *   erroneously set to 1 can damage the SoC!
+	 *   => TF-A enables the low power mode only if VDD < 2.7V (in DT)
+	 *      but this value needs to be consistent with board design.
+	 */
+	res = stm32_bsec_find_otp_in_nvmem_layout("hw2_otp", &otp_id, NULL);
+	if (res)
+		panic();
+
+	res = stm32_bsec_read_otp(&otp_value, otp_id);
+	if (res)
+		panic();
+
+	product_below_2v5 = ((otp_value & HW2_OTP_PRODUCT_BELOW_2V5) != 0);
+
+	/* Get VDD supply */
+	regu = regulator_get_by_regulator_name("vdd");
+	if (!regu)
+		panic("VDD regulator not found");
+
+	res = regulator_get_voltage(regu, &vdd_voltage);
+	if (res || !vdd_voltage)
+		panic("VDD get voltage error");
+
+	/* Check if VDD is Low Voltage */
+	if (vdd_voltage < 2700U) {
+		vdd_low_voltage = true;
+		enable_high_speed_mode_low_voltage();
+
+		if (!product_below_2v5)
+			IMSG("Product_below_2v5=0: HSLVEN protected by HW\n");
+
+	} else {
+		if (product_below_2v5) {
+			EMSG("Product_below_2v5=1:");
+			EMSG("\tHSLVEN update is destructive,");
+			EMSG("\tno update as VDD > 2.7V");
+			panic();
+		}
+	}
+
+	register_pm_driver_cb(syscfg_hslv_pm, NULL, "stm32mp1-hslv-driver");
+
+	return TEE_SUCCESS;
+}
+
+driver_init_late(stm32mp_syscfg_set_hslv);
