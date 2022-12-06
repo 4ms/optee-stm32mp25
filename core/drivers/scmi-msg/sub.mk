02935eb1356bfe6a0aff75f2140078e1ba40e1a4
Pascal Paillet <p.paillet@st.com>
2022-12-06 10:43:06 +0100

    drivers: scmi-msg: support performance domains for DVFS
    Implement some of the SCMI performance domain management messages
    in scmi-msg drivers to support basic DVFS scenario.
    
    Change-Id: I48d958389017229b91c2fb48a8f6ddf55bd7ddd0
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- core/drivers/scmi-msg/sub.mk
+++ core/drivers/scmi-msg/sub.mk
@@ -1,6 +1,7 @@
 srcs-y += base.c
 srcs-$(CFG_SCMI_MSG_CLOCK) += clock.c
 srcs-y += entry.c
+srcs-$(CFG_SCMI_MSG_PERF_DOMAIN) += perf_domain.c
 srcs-$(CFG_SCMI_MSG_REGULATOR_CONSUMER) += regulator_consumer.c
 srcs-$(CFG_SCMI_MSG_RESET_DOMAIN) += reset_domain.c
 srcs-$(CFG_SCMI_MSG_SHM_MSG) += shm_msg.c
