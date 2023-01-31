83e1eb64e90b9d5b65b8ca008390a668cc6903dd
Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
2023-01-31 09:53:45 +0100

    plat-stm32mp2: conf: enable PKA for stm32mp2 platforms
    Default enables stm32_pka and crypto framework for stm32mp2 platforms.
    
    Signed-off-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
    Change-Id: I96d241ffdcc9b87342616952ab3b57c57b5d936b
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/311871
    Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Domain-Review: Gatien CHEVALLIER <gatien.chevallier@st.com>

--- core/arch/arm/plat-stm32mp2/conf.mk
+++ core/arch/arm/plat-stm32mp2/conf.mk
@@ -95,6 +95,7 @@ CFG_STM32_I2C ?= y
 CFG_STM32_IAC ?= y
 CFG_STM32_IPCC ?= y
 CFG_STM32_OMM ?= y
+CFG_STM32_PKA ?= y
 CFG_STM32_PWR ?= y
 CFG_STM32_RIF ?= y
 CFG_STM32_RIFSC ?= y
@@ -125,6 +126,7 @@ CFG_EXTERNAL_DT ?= n
 # If any crypto driver is enabled, enable the crypto-framework layer
 ifeq ($(call cfg-one-enabled, CFG_STM32_CRYP \
 			      CFG_STM32_HASH \
+			      CFG_STM32_PKA \
 			      CFG_STM32_SAES ),y)
 $(call force,CFG_STM32_CRYPTO_DRIVER,y)
 endif
