35c96d6839569e3656ad504eeb9839c7850d9ef3
Antonio Borneo <antonio.borneo@foss.st.com>
2023-05-05 17:26:16 +0200

    drivers: stm32_iwdg: don't refresh the watchdog on early interrupt
    Both the bit to acknowledge the early interrupt and the field to
    set the window counter are in the same register.
    A write to the window counter's field triggers a watchdog refresh.
    
    To prevent the undesired watchdog refresh while acknowledging the
    early interrupt, write protect all the registers. This makes
    read-only all the register's fields except the bit to acknowledge
    the early interrupt.
    
    While there, add a check for spurious interrupts.
    
    Signed-off-by: Antonio Borneo <antonio.borneo@foss.st.com>
    Change-Id: I11c488e4ae0530c30ea44c9aecbda59ad79f360c
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/304614
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Tested-by: Antonio Maria BORNEO <antonio.borneo@st.com>
    Reviewed-by: Antonio Maria BORNEO <antonio.borneo@st.com>
    Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
    Domain-Review: Lionel DEBIEVE <lionel.debieve@foss.st.com>

--- core/drivers/stm32_iwdg.c
+++ core/drivers/stm32_iwdg.c
@@ -38,6 +38,7 @@
 #define IWDG_EWCR_OFFSET	U(0x14)
 #define IWDG_VERR_OFFSET	U(0x3F4)
 
+#define IWDG_KR_WPROT_KEY	U(0x0000)
 #define IWDG_KR_ACCESS_KEY	U(0x5555)
 #define IWDG_KR_RELOAD_KEY	U(0xAAAA)
 #define IWDG_KR_START_KEY	U(0xCCCC)
@@ -54,6 +55,7 @@
 #define IWDG_SR_UPDATE_MASK	(IWDG_SR_PVU | IWDG_SR_RVU | IWDG_SR_WVU | \
 				 IWDG_SR_EWU)
 #define IWDG_SR_ONF		BIT(8)
+#define IWDG_SR_EWIF		BIT(14)
 
 #define IWDG_EWCR_EWIE		BIT(15)
 #define IWDG_EWCR_EWIC		BIT(14)
@@ -169,7 +171,20 @@ static enum itr_return stm32_iwdg_it_handler(struct itr_handler *h)
 
 	clk_enable(iwdg->clock);
 
-	/*  Disable early interrupt */
+	/* Check for spurious interrupt */
+	if (!(io_read32(iwdg_base + IWDG_SR_OFFSET) & IWDG_SR_EWIF)) {
+		clk_disable(iwdg->clock);
+		return ITRR_NONE;
+	}
+
+	/*
+	 * Writing IWDG_EWCR_EWIT triggers a watchdog refresh.
+	 * To prevent the watchdog refresh, write-protect all the registers;
+	 * this makes read-only all IWDG_EWCR fields except IWDG_EWCR_EWIC.
+	 */
+	io_write32(iwdg_base + IWDG_KR_OFFSET, IWDG_KR_WPROT_KEY);
+
+	/* Disable early interrupt */
 	io_setbits32(iwdg_base + IWDG_EWCR_OFFSET, IWDG_EWCR_EWIC);
 
 	clk_disable(iwdg->clock);
