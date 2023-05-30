df972dcd373cecf4e1e8c19fd54b84df728aff93
Christophe Kerello <christophe.kerello@foss.st.com>
2023-05-30 17:21:54 +0200

    drivers: stm32_fmc: add FMC driver
    Implement RIF configuration for FMC, which is a RIF aware IP.
    It means that the FMC driver is in charge of configuring its own RIF
    restrictions and that the FMC has dedicated RIF configuration registers.
    
    Change-Id: I0c54e54f55984bf106bc8936c3d3a3e1ed097e78
    Signed-off-by: Gatien Chevallier <gatien.chevallier@st.com>
    Signed-off-by: Christophe Kerello <christophe.kerello@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/309744
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Tested-by: Christophe KERELLO <christophe.kerello@st.com>
    Reviewed-by: Christophe KERELLO <christophe.kerello@st.com>
    Domain-Review: Christophe KERELLO <christophe.kerello@st.com>

--- core/drivers/sub.mk
+++ core/drivers/sub.mk
@@ -35,6 +35,7 @@ srcs-$(CFG_STM32_BSEC) += stm32_bsec.c
 srcs-$(CFG_STM32_BSEC3) += stm32_bsec3.c
 srcs-$(CFG_STM32_CPU_OPP) += stm32_cpu_opp.c
 srcs-$(CFG_STM32_EXTI) += stm32_exti.c
+srcs-$(CFG_STM32_FMC) += stm32_fmc.c
 srcs-$(CFG_STM32_GPIO) += stm32_gpio.c
 srcs-$(CFG_STM32_HPDMA) += stm32_hpdma.c
 srcs-$(CFG_STM32_HSEM) += stm32_hsem.c
