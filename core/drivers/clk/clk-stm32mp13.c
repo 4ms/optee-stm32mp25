SPACE-Yonsei/myir-st-optee_os

clk: stm32mp13: do not reconfigure LSE
If LSE oscillator is already ON, which is the case when returning from
low-power state or if we are on VBAT, it mustn't be reconfigured.

Change-Id: I78ffdd951c0e83fe646a6ca9ee502c999796e3d8
Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/245518
Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>
Reviewed-by: Nicolas LE BAYON <nicolas.le.bayon@st.com>
Reviewed-by: Lionel DEBIEVE <lionel.debieve@foss.st.com>

core/drivers/clk/clk-stm32mp13.c
@@ -776,6 +776,9 @@ static void stm32_enable_oscillator_lse(struct clk_stm32_priv *priv,
 	if (osci->freq == 0U)
 		return;
 
+	if (clk_stm32_is_enabled_gate(osc_data->gate_id))
+		return;
+
 	clk_oscillator_set_bypass(priv, osc_data, osci->digbyp, osci->bypass);
 
 	clk_oscillator_set_drive(priv, osc_data,  osci->drive);

4ms/optee-stm32mp25

core/drivers/clk/clk-stm32mp13.c
@@ -777,6 +777,9 @@ static void stm32_enable_oscillator_lse(struct clk_stm32_priv *priv,
 	if (osci->freq == 0U)
 		return;
 
+	if (stm32_gate_is_enabled(osc_data->gate_id))
+		return;
+
 	clk_oscillator_set_bypass(priv, osc_data, osci->digbyp, osci->bypass);
 
 	clk_oscillator_set_drive(priv, osc_data,  osci->drive);