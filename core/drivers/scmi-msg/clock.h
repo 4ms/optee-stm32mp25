SPACE-Yonsei/myir-st-optee_os

plat-stm32mp1: scmi: round rate for LTDC clock
Implement round rate ops for LTDC clock.

Note that we put step_size to 0 for the Kernel to call this ops.
Otherwise the round rate of LTDC (PLL4_Q clocks) will always identical
with the request rate.

Change-Id: I6b9a82c307c1cafc1fc411e01f34dfd27ee0dff0
Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>

core/drivers/scmi-msg/clock.h
@@ -24,6 +24,7 @@ enum scmi_clock_command_id {
 	SCMI_CLOCK_RATE_GET = 0x006,
 	SCMI_CLOCK_CONFIG_SET = 0x007,
 	SCMI_CLOCK_DUTY_CYCLE_GET = 0x008,
+	SCMI_CLOCK_ROUND_RATE_GET = 0x009,
 };
 
 /* Protocol attributes */

4ms/optee-stm32mp25

core/drivers/scmi-msg/clock.h
@@ -24,6 +24,7 @@ enum scmi_clock_command_id {
 	SCMI_CLOCK_RATE_GET = 0x006,
 	SCMI_CLOCK_CONFIG_SET = 0x007,
 	SCMI_CLOCK_DUTY_CYCLE_GET = 0x00B,
+	SCMI_CLOCK_ROUND_RATE_GET = 0x00C,
 };
 
 /* Protocol attributes */