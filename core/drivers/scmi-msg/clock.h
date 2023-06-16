SPACE-Yonsei/myir-st-optee_os

plat-stm32mp1: implement duty cycle ops with SCMI clocks
Some driver from non secure need to known the duty cycle value of
their clock.

Change-Id: I8ae7b13329ffa663be41672932b4893b7facc5b7
Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>

core/drivers/scmi-msg/clock.h
@@ -23,6 +23,7 @@ enum scmi_clock_command_id {
 	SCMI_CLOCK_RATE_SET = 0x005,
 	SCMI_CLOCK_RATE_GET = 0x006,
 	SCMI_CLOCK_CONFIG_SET = 0x007,
+	SCMI_CLOCK_DUTY_CYCLE_GET = 0x008,
 };
 
 /* Protocol attributes */
@@ -56,6 +57,16 @@ struct scmi_clock_rate_get_a2p {
 struct scmi_clock_rate_get_p2a {
 	int32_t status;
 	uint32_t rate[2];
+};
+
+struct scmi_clock_duty_cycle_get_a2p {
+	uint32_t clock_id;
+};
+
+struct scmi_clock_duty_cycle_get_p2a {
+	int32_t status;
+	uint32_t num;
+	uint32_t den;
 };
 
 /*

4ms/optee-stm32mp25

core/drivers/scmi-msg/clock.h
@@ -23,6 +23,7 @@ enum scmi_clock_command_id {
 	SCMI_CLOCK_RATE_SET = 0x005,
 	SCMI_CLOCK_RATE_GET = 0x006,
 	SCMI_CLOCK_CONFIG_SET = 0x007,
+	SCMI_CLOCK_DUTY_CYCLE_GET = 0x00B,
 };
 
 /* Protocol attributes */
@@ -56,6 +57,16 @@ struct scmi_clock_rate_get_a2p {
 struct scmi_clock_rate_get_p2a {
 	int32_t status;
 	uint32_t rate[2];
+};
+
+struct scmi_clock_duty_cycle_get_a2p {
+	uint32_t clock_id;
+};
+
+struct scmi_clock_duty_cycle_get_p2a {
+	int32_t status;
+	uint32_t num;
+	uint32_t den;
 };
 
 /*