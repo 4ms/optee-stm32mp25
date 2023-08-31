de956173f3f03b7bfc54a986c471c86596b37b31
Patrick Delaunay <patrick.delaunay@foss.st.com>
2023-08-31 13:55:39 +0200

    driver: clk: rcc: restore clock for standby
    Correctly handle during standby of clock activated by RCC with pm function.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Change-Id: I03cb664f4e6cfb9a0c9b04ab89dcf358a80f9f8f
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/326485
    Tested-by: Gabriel FERNANDEZ <gabriel.fernandez@foss.st.com>
    Reviewed-by: Gabriel FERNANDEZ <gabriel.fernandez@foss.st.com>
    Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- core/drivers/clk/clk-stm32-rcc.c
+++ core/drivers/clk/clk-stm32-rcc.c
@@ -3,11 +3,44 @@
  * Copyright (C) 2023, STMicroelectronics - All Rights Reserved
  */
 
+#include <config.h>
 #include <drivers/clk_dt.h>
 #include <drivers/stm32_gpio.h>
 #include <kernel/dt.h>
+#include <kernel/pm.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <tee_api_defines_extensions.h>
+
+struct stm32mp_rcc_clock {
+	struct clk *clk;
+
+	SLIST_ENTRY(stm32mp_rcc_clock) link;
+};
+
+static SLIST_HEAD(, stm32mp_rcc_clock) stm32mp_rcc_list =
+	SLIST_HEAD_INITIALIZER(stm32mp_rcc_list);
+
+static TEE_Result
+stm32mp_rcc_pm(enum pm_op op, unsigned int pm_hint __unused,
+	       const struct pm_callback_handle *pm_handle __unused)
+{
+	struct stm32mp_rcc_clock *rcc_clock = NULL;
+
+	SLIST_FOREACH(rcc_clock, &stm32mp_rcc_list, link) {
+		if (op == PM_OP_RESUME) {
+			if (clk_enable(rcc_clock->clk)) {
+				EMSG("Can't enable %s clock",
+				     rcc_clock->clk->name);
+				panic();
+			}
+		} else {
+			clk_disable(rcc_clock->clk);
+		}
+	}
+
+	return TEE_SUCCESS;
+}
 
 static TEE_Result stm32mp_rcc_probe(const void *fdt, int node,
 				    const void *compat_data __unused)
@@ -27,9 +60,23 @@ static TEE_Result stm32mp_rcc_probe(const void *fdt, int node,
 
 	/* Enable clock(s) if present */
 	while (!clk_dt_get_by_index(fdt, node, idx++, &clk)) {
+		struct stm32mp_rcc_clock *rcc_clock = NULL;
+
 		if (clk_enable(clk))
 			panic("Can't enable RCC clock");
+
+		rcc_clock = calloc(1, sizeof(*rcc_clock));
+		if (!rcc_clock)
+			panic("Out of memory");
+
+		rcc_clock->clk = clk;
+
+		SLIST_INSERT_HEAD(&stm32mp_rcc_list, rcc_clock, link);
 	}
+
+	if (IS_ENABLED(CFG_PM))
+		register_pm_core_service_cb(stm32mp_rcc_pm, NULL,
+					    "stm32mp-rcc");
 
 	return TEE_SUCCESS;
 }
