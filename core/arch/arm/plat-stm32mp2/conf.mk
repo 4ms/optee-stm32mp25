3032fcbac00a12d2e15fa46a2dae10c49edb2c3d
Fabrice Gasnier <fabrice.gasnier@foss.st.com>
2023-09-05 18:29:25 +0200

    plat-stm32mp2: conf: enable lptimer driver
    Add STM32_LPTIMER driver support on stm32mp2 platforms.
    It depends on the counter driver. Therefore, enable it if
    the STM32_LPTIMER driver is enabled.
    
    Signed-off-by: Fabrice Gasnier <fabrice.gasnier@foss.st.com>
    Change-Id: Ia562deb9bca51e48210b06114ebc1a965d61df6d
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/326259
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
    Domain-Review: Gatien CHEVALLIER <gatien.chevallier@st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

--- core/arch/arm/plat-stm32mp2/conf.mk
+++ core/arch/arm/plat-stm32mp2/conf.mk
@@ -95,6 +95,7 @@ CFG_STM32_I2C ?= y
 CFG_STM32_IAC ?= y
 CFG_STM32_IPCC ?= y
 CFG_STM32_IWDG ?= y
+CFG_STM32_LPTIMER ?= y
 CFG_STM32_OMM ?= y
 CFG_STM32_PKA ?= y
 CFG_STM32_PWR ?= y
@@ -216,4 +217,9 @@ CFG_STPMIC2 ?= y
 CFG_WDT ?= $(CFG_STM32_IWDG)
 ifeq ($(CFG_WDT),y)
 $(call force,CFG_WDT_SM_HANDLER,y,Mandated by CFG_WDT)
+endif
+
+# Enable associated counter driver
+ifeq ($(CFG_STM32_LPTIMER),y)
+$(call force,CFG_COUNTER_DRIVER,y)
 endif
