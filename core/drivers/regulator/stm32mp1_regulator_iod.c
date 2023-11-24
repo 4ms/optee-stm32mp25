4ms/optee-stm32mp25

commit 5c1b56221a42de0acd9ca8ebb2a00f98f760b553

drivers: regulator: do not set regulator voltage if not implemented
Discriminate TEE_ERROR_NOT_IMPLEMENTED from other errors when trying
to set a regulator voltage.

Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Change-Id: Iae11ebca9171eecd1a92f8c5aa30f0b008fdcc00
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/322204
Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Tested-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Reviewed-by: Etienne CARRIERE <etienne.carriere@foss.st.com>
Domain-Review: Gatien CHEVALLIER <gatien.chevallier@st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

core/drivers/regulator/stm32mp1_regulator_iod.c
@@ -146,9 +146,11 @@ static TEE_Result iod_set_voltage(const struct regul_desc *desc, uint16_t mv)
 
 	/* Forward set voltage request to the power supply */
 	res = regulator_set_voltage(iod->supply, mv);
-	if (res) {
+	if (res && res != TEE_ERROR_NOT_IMPLEMENTED) {
 		EMSG("regulator %s set voltage failed", desc->node_name);
 		return res;
+	} else {
+		res = TEE_SUCCESS;
 	}
 
 	if (mv < IO_VOLTAGE_THRESHOLD)

commit 07d4421d46f39b14a6f36d8894589aaf7b10830a

drivers: regulator: restore IO when regulator voltage if not updated
Treat errors when trying to set an regulator voltage on power supply
and restore IO configuration for current voltage.

Fixes: 5c1b56221a42 ("drivers: regulator: do not set regulator voltage if not implemented")
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Change-Id: I335487f57d2f6b058f3396eb0ecae096c8aac238
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/327958
Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

core/drivers/regulator/stm32mp1_regulator_iod.c
@@ -121,6 +121,7 @@ static TEE_Result iod_set_voltage(const struct regul_desc *desc, uint16_t mv)
 {
 	struct iod_regul *iod = (struct iod_regul *)desc->driver_data;
 	TEE_Result res = TEE_ERROR_GENERIC;
+	TEE_Result result = TEE_ERROR_GENERIC;
 	bool en = false;
 
 	FMSG("%s: set volt to %"PRIu16" mv", desc->node_name, mv);
@@ -145,12 +146,13 @@ static TEE_Result iod_set_voltage(const struct regul_desc *desc, uint16_t mv)
 		return res;
 
 	/* Forward set voltage request to the power supply */
-	res = regulator_set_voltage(iod->supply, mv);
-	if (res && res != TEE_ERROR_NOT_IMPLEMENTED) {
-		EMSG("regulator %s set voltage failed", desc->node_name);
-		return res;
-	} else {
-		res = TEE_SUCCESS;
+	result = regulator_set_voltage(iod->supply, mv);
+	if (result) {
+		EMSG("regulator %s set voltage failed (%d)",
+		     desc->node_name, result);
+		res = regulator_get_voltage(iod->supply, &mv);
+		if (res)
+			return res;
 	}
 
 	if (mv < IO_VOLTAGE_THRESHOLD)
@@ -162,7 +164,7 @@ static TEE_Result iod_set_voltage(const struct regul_desc *desc, uint16_t mv)
 			return res;
 	}
 
-	return res;
+	return result;
 }
 
 static TEE_Result iod_list_voltages(const struct regul_desc *desc,