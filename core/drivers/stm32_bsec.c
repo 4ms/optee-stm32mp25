4ms/optee-stm32mp25

commit 426d40b7b43d865e4ffce68edaed9096b8f90a8c

drivers: stm32_bsec: update for stm32mp13
Some updates in BSEC driver for stm32mp13 support with
a specific compatible "st,stm32mp13-bsec":
- the OTP_SECURED device are defined in OTP0 of BSEC with:
  * STM32MP15x: bit 6
  * STM32MP13x: 0b111111 = 0x3F
- add the OTP for encryption key support on STMP32MP13x
- permanent lock status is updated without reset on STMP32MP13x

Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/219389
Change-Id: I316dacd7cb14ced0fd5ee15a4f827f075a605884

core/drivers/stm32_bsec.c
@@ -22,6 +22,13 @@
 #include <types_ext.h>
 #include <util.h>
 
+#ifdef CFG_STM32MP13
+#define DT_BSEC_COMPAT "st,stm32mp13-bsec"
+#endif
+#ifdef CFG_STM32MP15
+#define DT_BSEC_COMPAT "st,stm32mp15-bsec"
+#endif
+
 #define BSEC_OTP_MASK			GENMASK_32(4, 0)
 #define BSEC_OTP_BANK_SHIFT		U(5)
 
@@ -445,6 +452,10 @@ TEE_Result stm32_bsec_permanent_lock_otp(uint32_t otp_id)
 	else
 		result = check_no_error(otp_id, false /* not-disturbed */);
 
+#ifdef CFG_STM32MP13
+	io_write32(base + BSEC_OTP_CTRL_OFF, addr | BSEC_READ | BSEC_LOCK);
+#endif
+
 	power_down_safmem();
 
 out:
@@ -628,6 +639,23 @@ TEE_Result stm32_bsec_get_state(uint32_t *state)
 			*state = BSEC_STATE_SEC_OPEN;
 	}
 
+	if (IS_ENABLED(CFG_STM32MP13)) {
+		unsigned int start = OEM_ENC_KEY_OTP_BASE;
+		unsigned int end = start + OEM_ENC_KEY_OTP_COUNT;
+		unsigned int idx = 0;
+
+		for (idx = start; idx < end; idx++) {
+			TEE_Result res = TEE_ERROR_GENERIC;
+			bool locked = false;
+
+			res = stm32_bsec_read_sp_lock(idx, &locked);
+			if (res || !locked)
+				return TEE_SUCCESS;
+		}
+
+		*state |= BSEC_HARDWARE_KEY;
+	}
+
 	return TEE_SUCCESS;
 }
 
@@ -716,7 +744,7 @@ static void initialize_bsec_from_dt(void)
 	struct dt_node_info bsec_info = { };
 
 	fdt = get_embedded_dt();
-	node = fdt_node_offset_by_compatible(fdt, 0, "st,stm32mp15-bsec");
+	node = fdt_node_offset_by_compatible(fdt, 0, DT_BSEC_COMPAT);
 	if (node < 0)
 		panic();
 

core/arch/arm/plat-stm32mp1/platform_config.h
@@ -146,16 +146,33 @@
 #define UID1_OTP			14
 #define UID2_OTP			15
 #define HW2_OTP				18
+#define OEM_ENC_KEY_OTP			92
 
 /* Bit map for BSEC word CFG0_OTP */
+#ifdef CFG_STM32MP13
+#define CFG0_OTP_BIT_LENGTH			U(16)
+#define CFG0_OTP_MODE_MASK			GENMASK_32(9, 0)
+#define CFG0_OTP_MODE_SHIFT			U(0)
+#define CFG0_OPEN_DEVICE			U(0x17)
+#define CFG0_CLOSED_DEVICE			U(0x3F)
+#define CFG0_CLOSED_DEVICE_NO_BOUNDARY_SCAN	U(0x17F)
+#define CFG0_CLOSED_DEVICE_NO_JTAG		U(0x3FF)
+#define CFG0_CLOSED_MASK			CFG0_CLOSED_DEVICE
+#endif
+#ifdef CFG_STM32MP15
 #define CFG0_OTP_BIT_LENGTH		U(8)
 #define CFG0_OTP_SECURED_POS		U(6)
 #define CFG0_CLOSED_MASK		BIT(CFG0_OTP_SECURED_POS)
+#endif
 
 /* Bit map for BSEC word HW2_OTP */
 #define HW2_OTP_IWDG_HW_ENABLE_SHIFT	U(3)
 #define HW2_OTP_IWDG_FZ_STOP_SHIFT	U(5)
 #define HW2_OTP_IWDG_FZ_STANDBY_SHIFT	U(7)
+
+/* OTP layout for OEM_ENC_KEY_OTP on STM32MP13x */
+#define OEM_ENC_KEY_OTP_BASE		U(92)
+#define OEM_ENC_KEY_OTP_COUNT		U(4)
 
 /* GIC resources */
 #define GIC_SIZE			0x2000

commit 344e69a4e731be6604d91cd226c0343cd008e7fb

drivers: stm32_bsec: remove support of BSEC_HARDWARE_KEY
Remove the support of BSEC_HARDWARE_KEY information, this information
is not required by non secure world, and the associated  code for STMP32MP13x
based on "oem_enc_key" for  encryption key support, because the hardware
unique key (HUK) is not the encryption key.

Fixes: 426d40b7b43d ("drivers: stm32_bsec: update for stm32mp13")
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Change-Id: I339d9666694e0963f2265c140cbc2360b5a44223
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/310687
Tested-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Domain-Review: Gatien CHEVALLIER <gatien.chevallier@st.com>

core/drivers/stm32_bsec.c
@@ -690,10 +690,6 @@ TEE_Result stm32_bsec_find_otp_by_phandle(const uint32_t phandle,
 
 TEE_Result stm32_bsec_get_state(uint32_t *state)
 {
-	uint32_t otp_enc_id = 0;
-	size_t otp_bit_len = 0;
-	TEE_Result res = TEE_SUCCESS;
-
 	if (!state)
 		return TEE_ERROR_BAD_PARAMETERS;
 
@@ -704,27 +700,6 @@ TEE_Result stm32_bsec_get_state(uint32_t *state)
 			*state = BSEC_STATE_SEC_CLOSED;
 		else
 			*state = BSEC_STATE_SEC_OPEN;
-	}
-
-	if (!IS_ENABLED(CFG_STM32MP13))
-		return TEE_SUCCESS;
-
-	res = stm32_bsec_find_otp_in_nvmem_layout("oem_enc_key", &otp_enc_id,
-						  NULL, &otp_bit_len);
-	if (!res && otp_bit_len) {
-		unsigned int start = otp_enc_id / BSEC_BITS_PER_WORD;
-		size_t otp_nb = ROUNDUP_DIV(otp_bit_len, BSEC_BITS_PER_WORD);
-		unsigned int idx = 0;
-
-		for (idx = start; idx < start + otp_nb; idx++) {
-			bool locked = false;
-
-			res = stm32_bsec_read_sp_lock(idx, &locked);
-			if (res || !locked)
-				return TEE_SUCCESS;
-		}
-
-		*state |= BSEC_HARDWARE_KEY;
 	}
 
 	return TEE_SUCCESS;

core/include/drivers/stm32_bsec.h
@@ -203,7 +203,6 @@ TEE_Result stm32_bsec_find_otp_by_phandle(const uint32_t phandle,
  *             00b: Sec Open
  *             01b: Sec Closed
  *             11b: Invalid
- *           [8]: Hardware Key set = 1b
  * Return a TEE_Result compliant status
  */
 TEE_Result stm32_bsec_get_state(uint32_t *state);
@@ -212,7 +211,5 @@ TEE_Result stm32_bsec_get_state(uint32_t *state);
 #define BSEC_STATE_SEC_CLOSED	U(0x1)
 #define BSEC_STATE_INVALID	U(0x3)
 #define BSEC_STATE_MASK		GENMASK_32(1, 0)
-
-#define BSEC_HARDWARE_KEY	BIT(8)
 
 #endif /*__STM32_BSEC_H*/