d3a5b20a9691660da6368b0ad9a69cc0ba7df71b
Dan Green <danngreen1@gmail.com>
2026-01-06 15:21:59 -0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
-export CROSS_COMPILE64=aarch64-none-linux-gnu-
-export CROSS_COMPILE=aarch64-none-linux-gnu-
-make PLATFORM=stm32mp2 CFG_EXT_DTS=../dt-stm32mp/stm32mp2/a35-td/optee CFG_EMBED_DTB_SOURCE_FILE=stm32mp257f-ev1-ca35tdcid-ostl.dts CFG_TEE_CORE_LOG_LEVEL=2 CFG_SCMI_SCPFW=n O=build all
+#!/bin/bash
+
+make PLATFORM=stm32mp2 \
+	CFG_EMBED_DTB_SOURCE_FILE=stm32mp257f-ev1.dts \
+	CFG_TEE_CORE_LOG_LEVEL=2 \
+	CFG_SCMI_SCPFW=n \
+	CROSS_COMPILE64=aarch64-none-elf- \
+	CROSS_COMPILE=aarch64-none-elf- \
+	O=../build-optee all
+
+#CFG_EXT_DTS=../dt-stm32mp/stm32mp2/a35-td/optee 
